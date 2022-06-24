072d1c8118289e3d3e183af2529e90329434d1c8
Alberto1404 <91409881+Alberto1404@users.noreply.github.com>
2022-06-24 12:03:53 +0200

    Update cldice.py

--- TopNet/clDice/cldice_loss/cldice.py
+++ TopNet/clDice/cldice_loss/cldice.py
@@ -54,7 +54,7 @@ class soft_dice_cldice(nn.Module):
     def forward(self, y_pred, y_true):
         
         y_pred = self.sigmoid(y_pred)
-
+        # MODS from original clDice: work with uniform-radii vessels for soft-skeletonization, if not, soft-skeletonization won't be able to estimate the skelton correctly
         y_true_skel = torch.zeros_like(y_true).to(device)
         y_pred_skel = torch.zeros_like(y_pred).to(device)
         for idx, (vol_true, vol_pred) in enumerate(zip(y_true, y_pred)): # Iterate in batch size
@@ -69,28 +69,4 @@ class soft_dice_cldice(nn.Module):
 
         return (1.0-self.alpha)*dice+self.alpha*cl_dice
         
-
-
-"""class soft_dice_cldice(nn.Module):
-    def __init__(self, iter_=16, alpha=0.5, smooth = 1., binary = True):
-        super(soft_dice_cldice, self).__init__()
-        self.iter = iter_
-        self.smooth = smooth
-        self.alpha = alpha
-        self.binary = binary
-        self.sigmoid = nn.Sigmoid()
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, y_true, y_pred):
-        if self.binary:
-            y_pred = self.sigmoid(y_pred)
-        else:
-            y_pred = self.softmax(y_pred)
-        dice = soft_dice(y_true, y_pred)
-        skel_pred = soft_skel(y_pred, self.iter)
-        skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true))+self.smooth)/(torch.sum(skel_pred)+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred))+self.smooth)/(torch.sum(skel_true)+self.smooth)    
-        cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
-        return (1.0-self.alpha)*dice+self.alpha*cl_dice"""
 
